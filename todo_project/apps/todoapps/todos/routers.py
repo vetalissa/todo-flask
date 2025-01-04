951b71517740052a2bc6d39e8474bd7c6a252669
Elizaveta Dostovalova <lizakot20@gmail.com>
2025-01-04 14:50:09 +0300

    create delete and done func for todo

--- todo_project/apps/todoapps/todos/routers.py
+++ todo_project/apps/todoapps/todos/routers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template, request
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from apps.app import db
@@ -11,7 +11,7 @@ todos = Blueprint('todos', __name__, template_folder='templates')
 @login_required
 def view_todo():
     user_id = current_user.id
-    todo_list = Todo.query.filter(Todo.user_id == user_id and Todo.done == False).all()
+    todo_list = Todo.query.filter(Todo.user_id == user_id, Todo.done == False).all()
 
     return render_template('todos/list_todo.html', todo_list=todo_list)
 
@@ -20,7 +20,7 @@ def view_todo():
 @login_required
 def completed():
     user_id = current_user.id
-    todo_list = Todo.query.filter(Todo.user_id == user_id and Todo.done == True).all()
+    todo_list = Todo.query.filter(Todo.user_id == user_id, Todo.done == True).all()
 
     return render_template('todos/completed.html', todo_list=todo_list)
 
@@ -30,8 +30,6 @@ def completed():
 def create():
     if request.method == 'POST':
         try:
-            print(request.form.keys())
-            print(request.form)
             user_id = current_user.id
             title = request.form.get('title')
             description = request.form.get('description')
@@ -44,7 +42,46 @@ def create():
             db.session.add(todo)
             db.session.commit()
             flash('Задача добавлена!', category='success')
-        except Exception as e:
-            flash(f'Извините, произошла ошибка. Пробуйте снова. {e}', category='error')
+        except Exception:
+            flash('Извините, произошла ошибка. Пробуйте снова.', category='error')
 
     return render_template('todos/create.html')
+
+
+@todos.route('/done/<int:todo_id>/<string:redirect_to>')
+@login_required
+def done(todo_id, redirect_to):
+    try:
+        user_id = current_user.id
+        todo = Todo.query.filter(Todo.id == todo_id and Todo.user_id == user_id).first()
+        if todo.done is True:
+            todo.done = False
+        else:
+            todo.done = True
+
+        db.session.commit()
+    except Exception:
+        flash('Извините, произошла ошибка. Пробуйте снова.', category='error')
+
+    if redirect_to == 'view_todo':
+        return redirect(url_for('todos.view_todo'))
+    elif redirect_to == 'completed':
+        return redirect(url_for('todos.completed'))
+
+
+@todos.route('/delete/<int:todo_id>/<string:redirect_to>')
+@login_required
+def delete(todo_id, redirect_to):
+    try:
+        user_id = current_user.id
+        Todo.query.filter(Todo.id == todo_id and Todo.user_id == user_id).delete()
+
+        db.session.commit()
+        flash('Задача удалена!', category='success')
+    except Exception:
+        flash('Извините, произошла ошибка. Пробуйте снова.', category='error')
+
+    if redirect_to == 'view_todo':
+        return redirect(url_for('todos.view_todo'))
+    elif redirect_to == 'completed':
+        return redirect(url_for('todos.completed'))
